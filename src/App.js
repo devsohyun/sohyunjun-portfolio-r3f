30411ab3943c38c9764a172c5fa3882738542199
devsohyun <iamsohyunjun@gmail.com>
2022-12-12 12:18:49 +0900

    shaders init

--- src/App.js
+++ src/App.js
@@ -2,22 +2,34 @@
 import { useRef } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { ScrollControls, Scroll } from '@react-three/drei'
+import { useControls } from 'leva'
 import Images from './Images.js'
+import { Particles } from './Particles.js'
 
 export default function App()
 {
-    return <>
-        <Canvas gl={{ antialias: false }} dpr={[1, 1.5]}>
+    const props = useControls({
+        focus: { value: 5.1, min: 3, max: 7, step: 0.01 },
+        speed: { value: 100, min: 0.1, max: 100, step: 0.1 },
+        aperture: { value: 1.8, min: 1, max: 5.6, step: 0.1 },
+        fov: { value: 50, min: 0, max: 200 },
+        curl: { value: 0.25, min: 0.01, max: 0.5, step: 0.01 }
+    })
 
-            <ScrollControls damping={4} pages={4}>
-                <Scroll>
-                    <Images />
-                </Scroll>
-                <Cube />
-            </ScrollControls>
-            
-        </Canvas>
-    </>
+    return (
+        <>
+            <Canvas gl={{ antialias: false }} dpr={[1, 1.5]}>
+        
+                <ScrollControls damping={4} pages={4}>
+                    <Scroll>
+                        <Images />
+                    </Scroll>
+                    <Particles {...props} />
+                </ScrollControls>
+                
+            </Canvas>
+        </>
+    )
 }
 
 
